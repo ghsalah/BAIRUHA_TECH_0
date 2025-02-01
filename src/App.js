095d76e0d4b34e43c603341229ed3ca41a560548
ghsalah <salah121sias@gmail.com>
2025-02-01 16:56:31 +0530

    Updated App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import './App.css';
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="App">
+      <h1 className="hello-text">Hello</h1>
+    </div>
+  );
 }
 
 export default App;
